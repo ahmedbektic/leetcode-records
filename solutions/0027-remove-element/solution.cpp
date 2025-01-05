4e791f5bd096ada4ccaa4c7b5a1696cdde30f3fd
ahmedbektic <ahmed.bektic5@gmail.com>
2025-01-05 16:35:22 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 11.6 MB (83.00%)

---
+++ solutions/0027-remove-element/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int valid = 0; // Tracks the position for non-`val` elements
+        
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != val) {
+                nums[valid] = nums[i];
+                valid++;
+            }
+        }
+        
+        return valid;
+    }
+};
+
