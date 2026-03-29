e860fee669722d6a0521bb57c3666741306a29e6
ahmedbektic <ahmed.bektic5@gmail.com>
2026-03-29 21:20:28 +0000

    [LeetCode Sync] Runtime - 1527 ms (65.49%), Memory - 12.9 MB (10.45%)

---
+++ solutions/0300-longest-increasing-subsequence/solution.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        dp = [1] * len(nums)
+
+        for i in range( len(nums) - 1 , -1 , -1 ):
+            for j in range( len(nums) - 1 , i, -1 ):
+                if nums[i] < nums[j]:
+                    if (dp[j] + 1) > dp[i]:
+                        dp[i] = (dp[j] + 1)
+        
+        return max(dp)
+
+        
+        
